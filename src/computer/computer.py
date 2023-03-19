b56360b20c648f8d932e89986bb9febe29bf878f
Hyper <82003164+hyperrrrrrr@users.noreply.github.com>
2023-03-19 15:05:38 -0500

    works

--- src/computer/computer.py
+++ src/computer/computer.py
@@ -1,5 +1,6 @@
 from src.game.const import *
 from stockfish import Stockfish
+from colorama import Fore
 stockfish = Stockfish(path=r"src\computer\engine.exe")
 
 class Computer:
@@ -12,4 +13,7 @@ class Computer:
         '''
         #uses stockfish 15.1.3 to calc best move - board fen needed
         stockfish.set_fen_position(fen)
-        return stockfish.get_best_move_time(FIND_TIME) 
+        try:
+            return stockfish.get_best_move_time(FIND_TIME) 
+        except:
+            print(Fore.BLUE + "CALCULATIONERROR", Fore.WHITE + "MOVE INVALID")
